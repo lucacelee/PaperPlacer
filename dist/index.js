b4fcb38d8f8b62751831b13c0c1f4f8bf15d164c
lucacelee <nikita.mamray2008@gmail.com>
2025-12-27 19:53:43 +0000

    Add a way to connect to MariaDB

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,41 @@
 "use strict";
-console.log("Hey!");
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mariadb_1 = __importDefault(require("mariadb"));
+require('dotenv').config();
+const pool = mariadb_1.default.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    trace: true // Development only!
+});
+function connect() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const conn = yield pool.getConnection();
+        try {
+            yield conn.query("USE paperplacer;");
+        }
+        finally {
+            conn.end();
+        }
+    });
+}
+try {
+    connect();
+}
+catch (error) {
+    console.log("Failed to connect to MariaDB. Error:\n" + error);
+}
+// End
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log("Hey!");
+import mariadb from 'mariadb';
+require('dotenv').config()
+
+const pool = mariadb.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  trace: true     // Development only!
+});
+
+async function connect() {
+  const conn = await pool.getConnection();
+  try {
+    await conn.query("USE paperplacer;");
+  } finally {
+    conn.end();
+  }
+}
+
+try {
+  connect();
+} catch (error) {
+  console.log("Failed to connect to MariaDB. Error:\n" + error);
+}
+
+
+// End
