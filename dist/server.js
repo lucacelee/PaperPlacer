1cf5835586424cc767b3bcb7662a014baaee70b6
lucacelee <nikita.mamray2008@gmail.com>
2025-12-28 23:37:43 +0000

    Let HTML be mirrorred tp dist/

--- dist/server.js
+++ dist/server.js
@@ -5,18 +5,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.webserver = void 0;
 const express_1 = __importDefault(require("express"));
+const render_1 = require("./render");
 class webserver {
     constructor() {
         this.server = (0, express_1.default)();
     }
     respond() {
         this.server.get('/', (request, response) => {
-            response.send("Hi!");
+            response.send((0, render_1.renderHtml)("index.html"));
         });
     }
     listen() {
         this.server.listen(3000, () => {
-            console.log("http://localhost:3000");
+            console.log("The webserver is accessible at http://localhost:3000");
         });
     }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,18 @@
 import express from "express";
+import { renderHtml } from "./render";
 
 export class webserver {
     server = express();
 
     respond () {
         this.server.get('/', (request, response) => {
-        response.send("Hi!");
+        response.send(renderHtml("index.html"));
         });
     }
 
     listen () {
         this.server.listen(3000, () => {
-        console.log("http://localhost:3000");
+        console.log("The webserver is accessible at http://localhost:3000");
         });
     }
 }
